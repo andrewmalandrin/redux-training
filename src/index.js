212056f5a88a90a8af3185bc24f2cfa52aecdc26
Andrew Malandrin <del_malandrin@hotmail.com>
2022-05-02 18:17:18 -0300

    React-Config

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as ReactDOM from "react-dom/client"
+
+const app = (
+        <div id="app">
+            
+        </div>
+    )
+
+
+
+const App = ReactDOM.createRoot(document.getElementById("app"))
+
+App.render(app)
